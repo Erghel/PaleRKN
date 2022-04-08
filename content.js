efbffbba471a60e2057c18045546c12eccc2e37e
Erghel <you@example.com>
2022-04-08 15:44:49 +0300

    Первый релиз

--- content.js
+++ content.js
@@ -1,10 +1,36 @@
-// yandex - class = "Warning-Item"
+// yandex - class = "Warning-Item", "Warning_type_roskomnadzor"
 // mailru - class = "App-RKN"
 
-if (window.location.host == "go.mail.ru") {
-    //
-}
+// if (window.location.host == "go.mail.ru") {
+//     let element = document.getElementsByClassName("App-RKN");
+// while (element.length) {
+//   element[0].parentNode.removeChild(element[0]);
+// }
+// }
 
-if (window.location.host == "yandex.ru") {
-    //
+// if (window.location.host == "yandex.ru") {
+//     let element = document.getElementsByClassName("Warning_type_roskomnadzor");
+// while (element.length) {
+//   element[0].parentNode.removeChild(element[0]);
+// }
+// }
+
+let classForRemove;
+let element;
+ 
+function removeElement(elementClass) {
+  element = document.querySelectorAll(`.${classForRemove}`);
+  element.forEach((elem) => {
+    elem.parentNode.removeChild(elem);
+  });
 }
+ 
+if (window.location.host == "go.mail.ru") {
+  classForRemove = "App-RKN";
+}
+ 
+if (window.location.host == "yandex.ru") {
+  classForRemove = "Warning_type_roskomnadzor";
+}
+ 
+if (classForRemove) removeElement();
