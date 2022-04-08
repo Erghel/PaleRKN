b11716588dae8bfd2ba7487740fbfecb81023b39
Erghel <you@example.com>
2022-04-08 15:24:20 +0300

    исправил ошибку

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 // yandex - class = "Warning-Item"
 // mailru - class = "App-RKN"
 
-if (window.location.href == "go.mail.ru") {
+if (window.location.host == "go.mail.ru") {
     //
 }
 
-if (window.location.href == "yandex.ru") {
+if (window.location.host == "yandex.ru") {
     //
 }
